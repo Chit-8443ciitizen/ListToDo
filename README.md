6da2a326f004fd76a95c4d2a7970491b0090bace
Chit-8443ciitizen <hoahmpd06987@fpt.edu.vn>
2024-03-19 20:43:37 +0700

    build done UI

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+firebase deploy kk
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/connectDb/connect.tsx
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwp6XQOhJ6aEY8deKDQkF2mh5EbfTlJZ0",
+  authDomain: "listtodo-dd493.firebaseapp.com",
+  projectId: "listtodo-dd493",
+  storageBucket: "listtodo-dd493.appspot.com",
+  messagingSenderId: "527188258078",
+  appId: "1:527188258078:web:9bad1219ea69d9678f6d0f",
+  measurementId: "G-9XGVGT5DW5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const Connection = () =>{
+    return <>
+    kk</>
+}
+export default Connection
